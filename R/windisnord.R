9bda20cc6510bb9fb17a991909c2a8832377dc6b
Neal <nbarsch@colgate.edu>
2021-02-13 22:56:09 -0700

    Update windisnord.R

--- R/windisnord.R
+++ R/windisnord.R
@@ -6,10 +6,8 @@ windisnord <- function(){
   oldwd <- getwd()
   setwd("C:/Program Files/NordVPN")
   myiporig <- RCurl::getURL("ifconfig.me")
-  if(isTRUE(disconnect)){
-    system("nordvpn -d")
-    Sys.sleep(5)
-  }
+  system("nordvpn -d")
+  Sys.sleep(5)
   myip <- RCurl::getURL("ifconfig.me")
   setwd(oldwd)  
   print(paste0("IP CHANGED FROM: ",myiporig," TO: ",myip))
